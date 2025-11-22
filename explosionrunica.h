2fc88ec328df486aa8862ca1b0051bf780c61ef8
Juan Sebastian Vasquez <sebas1201@javerianacali.edu.co>
2025-11-22 17:55:05 -0500

    explosionrunica.h

--- explosionrunica.h
+++ explosionrunica.h
@@ -1,17 +1,18 @@
 #ifndef EXPLOSIONRUNICA_H
 #define EXPLOSIONRUNICA_H
 #include "Objeto.h"
+#include "Entidad.h"
 
 class Jugador;
 
-class ExplosionRunica : public Objeto{
+class ExplosionRunica : public Objeto {
 private:
 	int danio = 160;
 	int gastoMana = 100;
 	std::string letraAsociada = "E";
 public:
 	ExplosionRunica(std::string nombre);
-	void usarObjeto(Jugador& jugador) const;
+	void usarObjeto(Jugador& jugador, Entidad& Enemigo) const;
 	std::string getLetra() const;
 };
 
